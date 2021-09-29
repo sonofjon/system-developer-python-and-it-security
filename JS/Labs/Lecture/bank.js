85a7cbb41c6bc5e4a09e2c05b75e695cb8a6628d
Andreas Jonsson <ajdev8@gmail.com>
2021-09-29 09:00:56 +0200

    JS: Lecture: Add bank and unit exercises

---
+++ JS/Labs/Lecture/bank.js
@@ -0,0 +1,3 @@
+alert("Welcome to bank");
+var Deposit = prompt("How much money would you like to deposit?");
+alert("Your total is " + Deposit);
---
+++ JS/Labs/Lecture/unit.js
@@ -0,0 +1,4 @@
+alert("Welcome to Weight Converter");
+var weightPounds = prompt("How many pounds [lbs] would you like to convert?");
+var weightKilograms = weightPounds / 2.205;
+alert("The weight in kilograms is " + weightKilograms + " kg.");
