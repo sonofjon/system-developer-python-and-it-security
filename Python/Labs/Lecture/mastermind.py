5d9bde68fbf66fbbf8ba51d102cb11a8cd0fefd1
Andreas Jonsson <ajdev8@gmail.com>
2021-10-10 10:23:09 +0200

    Code cleanup

--- Python/Labs/Lecture/mastermind.py
+++ Python/Labs/Lecture/mastermind.py
@@ -1,16 +1,18 @@
 import random
+
 def get_guess():
-    return list(input("What is your guess?"))
+    return list(input("What is your guess? "))
+
 def generate_code():
     digits = [str(num) for num in range(10)]
     print(digits, '<------')
     random.shuffle(digits)
     print(digits)
-
     return digits[:3]
+
 def generate_clues(code,userGuess):
-    #Takes in a user guess and code then compares the numbers in a loop and
-    #creates a list of clues according to the matching parameters.
+    # Takes in a user guess and code then compares the numbers in a loop and
+    # creates a list of clues according to the matching parameters.
 
     if userGuess == code:
         return "CODE CRACKED"
