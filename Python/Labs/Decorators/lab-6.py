5941397c8403b6cbdecf7d5355030eef2aaa58fc
Andreas Jonsson <ajdev8@gmail.com>
2021-10-11 10:11:35 +0200

    Python: Decorators: Update Lab 6

--- Python/Labs/Decorators/lab-6.py
+++ Python/Labs/Decorators/lab-6.py
@@ -1,18 +1,14 @@
-def decorator_function(original_function):
-    def wrapper_function (numbers):
-        print(numbers)
-        return original_function(numbers)
-    return wrapper_function
+def even_numbers(function):
+    def wrap(numbers):
+        evens = []
+        for num in numbers:
+            if num%2 == 0:
+                evens.append(num)
+        return function(evens)
+    return wrap
 
-@decorator_function   # identical to: even_numbers = decorator_func(even_numbers)
-def even_numbers(numbers):
-    evens = []
-    for num in numbers:
-        if num%2 == 0:
-            evens.append(num)
-    return evens
-    # print(evens)
+@even_numbers   # identical to: print_numbers = even_numbers(print_numbers)
+def print_numbers(numbers):
+    print(numbers)
 
-# even_numbers([1,2,3,4,5])
-print(even_numbers([1,2,3,4,5]))
-
+print_numbers([1,2,3,4,5])
