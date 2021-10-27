c58cd23ad7cca6ca1e45041bb35799f6b31f3fa8
Andreas Jonsson <ajdev8@gmail.com>
2021-10-27 10:22:16 +0200

    Django: Lab 4: Add Django environment to populate.py script
    This enables running the script as a regular python script.

--- Django/Labs/Lab 4/lab_4_project/populate.py
+++ Django/Labs/Lab 4/lab_4_project/populate.py
@@ -1,3 +1,11 @@
+import os
+import django
+
+# Initial setup to use Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab_4_project.settings")
+django.setup()
+
+# Imports must be placed after inital setup
 from user_app.models import User   # or user built-in User class
 
 u = User(first_name="First", last_name="Last", email="First.Last@domain.com")
