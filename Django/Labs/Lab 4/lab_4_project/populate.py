17340bbdb6ee60b862a6422a5d5ce5d7e30132f1
Andreas Jonsson <ajdev8@gmail.com>
2021-10-27 10:51:52 +0200

    Django: Lab 4: Use Faker to generate users
    This enables running the script as a regular python script.

--- Django/Labs/Lab 4/lab_4_project/populate.py
+++ Django/Labs/Lab 4/lab_4_project/populate.py
@@ -1,5 +1,6 @@
 import os
 import django
+from faker import Faker
 
 # Initial setup to use Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab_4_project.settings")
@@ -8,5 +9,8 @@ django.setup()
 # Imports must be placed after inital setup
 from user_app.models import User   # or user built-in User class
 
-u = User(first_name="First", last_name="Last", email="First.Last@domain.com")
-u.save()
+fake = Faker()
+number_of_users = 8
+for _ in range(number_of_users):
+    u = User(first_name=fake.first_name(), last_name=fake.last_name(), email=fake.ascii_email())
+    u.save()
