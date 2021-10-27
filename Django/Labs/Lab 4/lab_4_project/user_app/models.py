3427994eab4b3236ed424b5f2bfc1f609be4a332
Andreas Jonsson <ajdev8@gmail.com>
2021-10-27 17:05:24 +0200

    Django: Lab 4: Add str method to User class

--- Django/Labs/Lab 4/lab_4_project/user_app/models.py
+++ Django/Labs/Lab 4/lab_4_project/user_app/models.py
@@ -5,3 +5,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField()
+
+    def __str__(self):
+        """Returns a string represeantation of a user."""
+        return self.first_name + " " + self.last_name
