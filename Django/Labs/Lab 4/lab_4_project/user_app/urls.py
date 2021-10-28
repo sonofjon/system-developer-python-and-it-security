90796a0117588216479775217ea8b5f5f1c56231
Andreas Jonsson <ajdev8@gmail.com>
2021-10-28 11:49:06 +0200

    Django: Lab 4: Bug fix

--- Django/Labs/Lab 4/lab_4_project/user_app/urls.py
+++ Django/Labs/Lab 4/lab_4_project/user_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="user_app_index"),
-    path('user/', views.user, name="user_app_user"),
+    path('users/', views.users, name="user_app_users"),
 ]
