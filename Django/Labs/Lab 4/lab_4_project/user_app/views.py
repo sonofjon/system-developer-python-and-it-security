ef986be811f1ca8cc9af3ec2766fab5779d0d07b
Andreas Jonsson <ajdev8@gmail.com>
2021-10-28 11:39:28 +0200

    Django: Lab 4: Provide view for index page

--- Django/Labs/Lab 4/lab_4_project/user_app/views.py
+++ Django/Labs/Lab 4/lab_4_project/user_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "user_app/index.html")
+
 def users(request):
     context = {???}
     return render(request, "user_app/users.html", context)
