e46817fb19928611d81b125b2bb9abc89d36e64a
Andreas Jonsson <ajdev8@gmail.com>
2021-10-28 11:41:53 +0200

    Django: Lab 4: Provide context for call to users view

--- Django/Labs/Lab 4/lab_4_project/user_app/views.py
+++ Django/Labs/Lab 4/lab_4_project/user_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request, "user_app/index.html")
 
 def users(request):
-    context = {???}
+    context = {"users": User.objects.all()}
     return render(request, "user_app/users.html", context)
