7a0ab404ccd3ebcc5364a666c8101626fe6b3e9e
Andreas Jonsson <ajdev8@gmail.com>
2022-06-07 21:16:38 +0200

    IT Security: Sockets: Add latest available code (untested)

--- IT Security/Labs/Sockets/client.py
+++ IT Security/Labs/Sockets/client.py
@@ -1,28 +1,12 @@
 import socket
 
-HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.10.143"
+FILE = "sample.txt"
+ADDR = (SERVER, PORT)
 
-addr = (SERVER, PORT)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(addr)
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
-
-send("Hello World!")
-input()
-send("Hello Everyone!")
-input()
-send("Hello Tim!")
-
-send(DISCONNECT_MESSAGE)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect(ADDR)
+    with open(FILE, mode='rb') as file:
+        client.sendfile(file)
--- IT Security/Labs/Sockets/server.py
+++ IT Security/Labs/Sockets/server.py
@@ -1,47 +1,28 @@
 import socket
-# import threading
 
-HEADER = 64
+BUFSIZE = 16
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 
 host_name = socket.gethostname()
 server_name = socket.gethostbyname(host_name)
 addr = (server_name, PORT)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(addr)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(addr)
+    print("[STARTING] server is starting...")
+    s.listen()
+    print(f"[LISTENING] Server is listening on {server_name}")
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    while True:
+        conn, addr = s.accept()
+        print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
+        while True:
+            msg = conn.recv(BUFSIZE).decode(FORMAT)
+            if msg == '':
+                break
 
             print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
 
-    conn.close()
-
-
-def start(server):
-    server.listen()
-    print(f"[LISTENING] Server is listening on {server_name}")
-    while True:
-        conn, addr = server.accept()
-        handle_client(conn, addr)
-        # thread = threading.Thread(target=handle_client, args=(conn, addr))
-        # thread.start()
-        # print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
-
-print("[STARTING] server is starting...")
-start(s)
-
+        conn.close()
